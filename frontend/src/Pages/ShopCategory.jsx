43bab204afb20fd9a857cd7c07b2ce706be9a9b3
Abhishek-Chincholikar <9869abhishek@gmail.com>
2025-11-25 13:47:16 +0530

    Updated API URL to Render

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://quick-cart-backend-z224.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
