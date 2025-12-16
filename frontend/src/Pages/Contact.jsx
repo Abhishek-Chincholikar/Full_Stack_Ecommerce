236a1aba87da3afda458d76de3b12cf592a0e237
Rajshree <rajshreevc2018@gmail.com>
2025-12-16 16:05:12 +0530

    change

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -3,8 +3,8 @@ import './CSS/Contact.css'
 
 const Contact = () => {
   return (
-    <div className='contact'>
-      <div className="contact-container">
+    <div className='contact' >
+      <div className="contact-container" style={{padding:"50px"}}>
         <h1>Get in Touch</h1>
         <p>We'd love to hear from you! Send us a message below.</p>
         <div className="contact-fields">
